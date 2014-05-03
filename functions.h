04098aefc032817dcdb9a35c195ed526771cbe8f
wmurmann <wmurmann@gmail.com>
2014-05-02 21:56:38 -0700

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,24 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+#include <iostream>
+#include <string>
+#include <cctype>
 
+using namespace std;
+
+bool isNumb(string number)
+{
+	bool answ = true;
+	for(int i = 0; i < number.length(); i++)
+	{
+		if(isalpha(number[i]))
+		{
+			answ = false;
+		}
+	}
+	return answ;
+}
+
+
+
+#endif
